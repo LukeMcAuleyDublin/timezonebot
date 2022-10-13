d32c483acfb613f95fd2cdfb6ffeb1744829be70
Luke McAuley <55586654+LukeMcAuleyDublin@users.noreply.github.com>
2022-10-13 01:12:38 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,23 +9,39 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-intents = discord.Intents.none()
-intents.messages = True
+intents = discord.Intents.default()
 client = discord.Client(intents=intents)
-
-bot = commands.Bot(command_prefix='!', intents=intents)
 
 @client.event
 async def on_ready():
-    print('Hello!')
-
-@bot.command(name='timezones')
-async def timezones(ctx):
-    await ctx.send('Hello!')
+	print("Logged in as a bot {0.user}".format(client))
 
 @client.event
 async def on_message(message):
-    if 'hello' in message.content.lower():
-        await message.channel.send('Hello!')
+	username = str(message.author).split("#")[0]
+	channel = str(message.channel.name)
+	user_message = str(message.content)
 
-bot.run(TOKEN)
+	print(f'Message {user_message} by {username} on {channel}')
+    
+
+	if message.author == client:
+		return
+
+	if channel == "bot-channel":
+		if user_message.lower() == "hello" or user_message.lower() == "hi":
+			await message.channel.send(f'Hello {username}')
+			return
+		elif user_message.lower() == "bye":
+			await message.channel.send(f'Bye {username}')
+		elif user_message.lower() == "tell me a joke":
+			jokes = [" Can someone please shed more\
+			light on how my lamp got stolen?",
+					"Why is she called llene? She\
+					stands on equal legs.",
+					"What do you call a gazelle in a \
+					lions territory? Denzel."]
+			await message.channel.send(random.choice(jokes))
+
+
+client.run(TOKEN)
